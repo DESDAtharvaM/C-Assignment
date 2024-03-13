d0ee0fbfb33219a1e301f811aaeaad98e89a7316
MAtharva31 <31mahajanatharva@gmail.com>
2024-03-13 14:53:13 +0530

    Assignment1-Q1-A

---
+++ assgn1-a.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+
+{
+  int a,b,c;
+  printf("Enter a :\n");
+  scanf("%d", &a);
+
+  printf("Enter b :\n");
+  scanf("%d", &b);
+
+  c = a+b;
+  printf("sum of a and b : %d\n",c);
+  return 0;
+}
