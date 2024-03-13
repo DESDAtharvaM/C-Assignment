f61b4a949e525b3c74bcd31bb17019e4f66c537e
MAtharva31 <31mahajanatharva@gmail.com>
2024-03-13 15:21:47 +0530

    Assignment1-Q1(b)

---
+++ assgn1-b.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+
+{
+  int a,b,c;
+  printf("Enter a :\n");
+  scanf("%d", &a);
+
+  printf("Enter b :\n");
+  scanf("%d", &b);
+
+  c = a-b;
+  printf("Difference of a and b : %d\n",c);
+  return 0;
+}
---
+++ assgn1-c.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+
+{
+  int a,b,c;
+  printf("Enter a :\n");
+  scanf("%d", &a);
+
+  printf("Enter b :\n");
+  scanf("%d", &b);
+
+  c = a*b;
+  printf("a*b = %d",c);
+  return 0;
+}
