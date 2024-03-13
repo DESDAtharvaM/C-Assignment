f297c2f753447a993fe377a1a3a4f84f7753fcf7
MAtharva31 <31mahajanatharva@gmail.com>
2024-03-13 16:27:25 +0530

    Assignment1-Q2

---
+++ assgn1-Q2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+   int n;
+   scanf("%d", &n);
+   printf("Decimal value = %d",n);
+   printf("\n Octal value = %o",n);
+   printf("\n Hexadecimal  value = %x",n);
+   return 0;
+}
