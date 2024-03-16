a5dbedaccd6ace08758d1fc0132661a00cdba4e5
MAtharva31 <31mahajanatharva@gmail.com>
2024-03-16 17:17:15 +0530

    Assignment 2- Q7-a

---
+++ assgn2-Q7-a.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main() 
+
+{
+   int year;
+   printf("Enter the year : ");
+   scanf("%d", &year);
+
+   if(year / 4 * 4 == year && year / 100 * 100 != year || (year / 400 * 400 == year))
+        printf("Leap year \n");
+   else
+        printf("Not a Leap Year \n");
+   return 0;
+}   
+
