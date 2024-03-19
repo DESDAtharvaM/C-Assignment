33bd8ff22793046214234a071c0439c33abf4332
MAtharva31 <31mahajanatharva@gmail.com>
2024-03-19 16:38:17 +0530

    Assignment 4-Q1

---
+++ assgn4-Q1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int fact();
+int main()
+{
+    int c = 1, a, b, i = 1;
+    printf("Enter any no:");
+    scanf("%d", &a);
+    fact(a, c);
+
+    // printf("%d",c);
+}
+
+int fact(int a, int c)
+{
+    int i = 1;
+    while (i <= a)
+    {
+        c = c * i;
+        i++;
+    }
+    printf("%d\n", c);
+    return c;
+}
